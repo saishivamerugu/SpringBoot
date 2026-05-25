a8c4cd3b02118a177a71fffa8e7a4ee162d0b08d
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-05-25 23:09:53 +0530

    Add files via upload

---
+++ SpringSecurityInMemory/src/main/java/com/project/SpringSecurityInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityInMemoryApplication.class, args);
+	}
+
+}
