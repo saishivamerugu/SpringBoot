03954b13576c57bd936a80d03112b1f6d04810f5
Sai Shiva <146641544+saishivamerugu@users.noreply.github.com>
2026-05-28 23:33:18 +0530

    Update SecurityConfig.java

--- SpringSecurityJWT/src/main/java/com/project/security/SecurityConfig.java
+++ SpringSecurityJWT/src/main/java/com/project/security/SecurityConfig.java
@@ -1,30 +1,78 @@
 package com.project.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
-	 @Bean
-	 public PasswordEncoder passwordEncoder() {
-	    	return new BCryptPasswordEncoder();
-	    }
-	 
-	 @Bean
-	    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-	        http.authorizeHttpRequests(auth ->
-	                auth.requestMatchers("/signup")
-	                        .permitAll()
-	                        .anyRequest()
-	                        .authenticated())
-	                .formLogin(Customizer.withDefaults()) 
-	                .httpBasic(Customizer.withDefaults());
-	        return http.build();
-	    }
+	@Autowired
+	JWTAuthFilter jwtAuthFilter;
+
+	// Password encoder bean
+	// used to encrypt password before storing into database
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+			throws Exception {
+
+		http
+
+			// Disable csrf
+			// because we are using JWT token authentication
+			.csrf(csrf -> csrf.disable())
+
+			// Disable cors
+			.cors(cors -> cors.disable())
+
+			// Configure authorization rules
+			.authorizeHttpRequests(auth ->
+
+				auth
+
+				// signup and login should be public
+				.requestMatchers("/auth/**","/open")
+				.permitAll()
+
+				// except above all requests should be authenticated
+				.anyRequest()
+				.authenticated())
+
+			// JWT is stateless
+			// server should not create session
+			.sessionManagement(session ->
+				session.sessionCreationPolicy(
+						SessionCreationPolicy.STATELESS))
+
+			// Before UsernamePasswordAuthenticationFilter
+			// execute my JWTAuthFilter
+			.addFilterBefore(jwtAuthFilter,
+					UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(
+			AuthenticationConfiguration authConfig)
+			throws Exception { 
+
+		// AuthenticationManager is responsible
+		// for authenticating username and password
+
+		return authConfig.getAuthenticationManager();
+	}
 }
